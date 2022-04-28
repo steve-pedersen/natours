32218a3097f1fb2739c58e1d8b82046b42686245
stevedpedersen <stevedpedersen@gmail.com>
2022-04-28 10:35:34 -0700

    final project

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 (function() {
-  var closeNav = function() {
+  var clickNavBtn = () => {
     let button = document.getElementsByClassName('navigation__button')[0];
     button.click();
-  }
+  };
 
-  var navLinks = document.getElementsByClassName('navigation__link') ?? [];
+  var navLinks = document.getElementsByClassName('navigation__link');
 
   for (let i = 0; i < navLinks.length; i++) {
     let em = navLinks[i];
-    em.addEventListener('click', function(e) {
-      closeNav();
+    em.addEventListener('click', (e) => {
+      clickNavBtn();
     });
   }
+
+  document.getElementById('popup').addEventListener('click', (e) => {
+    let ems = document.getElementsByClassName('popup__close');
+    
+    for (let i = 0; i < ems.length; i++) {
+      ems[i].click();
+    }
+  });
+
 })();
